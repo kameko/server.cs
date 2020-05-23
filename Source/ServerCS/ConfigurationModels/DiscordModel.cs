aefce752683357700c7528eefdc91b6b9a60e4a3
Kameko Kirishiki <kameko.k@outlook.com>
2020-05-23 02:23:24 -0400

    config model factories

--- Source/ServerCS/ConfigurationModels/DiscordModel.cs
+++ Source/ServerCS/ConfigurationModels/DiscordModel.cs
@@ -5,9 +5,16 @@ namespace ServerCS.ConfigurationModels
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
     
     public class DiscordModel
     {
+        public static string ConfigurationKey { get; set; } = "Discord";
         public string TokenFilePath { get; set; } = string.Empty;
+        
+        public static DiscordModel FromConfiguration(IConfiguration configuration)
+        {
+            return configuration.GetSection(ConfigurationKey).Get<DiscordModel>();
+        }
     }
 }
--- Source/ServerCS/ConfigurationModels/ServerCSModel.cs
+++ Source/ServerCS/ConfigurationModels/ServerCSModel.cs
@@ -5,9 +5,17 @@ namespace ServerCS.ConfigurationModels
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
     
     public class ServerCSModel
     {
+        public static string ConfigurationKey { get; set; } = "ServerCS";
+        
         public string DataFolder { get; set; } = string.Empty;
+        
+        public static ServerCSModel FromConfiguration(IConfiguration configuration)
+        {
+            return configuration.GetSection(ConfigurationKey).Get<ServerCSModel>();
+        }
     }
 }
